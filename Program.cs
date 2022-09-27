43ba7dec22a9b6647a000f0cd8b3104cd14b5222
WCram <wcram@rtc.edu>
2022-09-26 18:13:58 -0700

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Character_Sheet_F22
+{
+    internal class Program
+    {
+        // Character Sheet - In Class Review
+        static void Main(string[] args)
+        {
+
+        } // Main
+
+    } // class
+
+} // namespace
---
+++ README.md
@@ -0,0 +1,2 @@
+# Character_Sheet_F22
+ Character Sheet - In Class Review - Programming 1
