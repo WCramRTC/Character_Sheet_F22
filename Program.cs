5211ee8784100809852ad32372d5e5a5f1df5c0a
WCram <wcram@rtc.edu>
2022-09-26 18:50:18 -0700

    Updated Character Sheet

--- Program.cs
+++ Program.cs
@@ -11,7 +11,27 @@ namespace Character_Sheet_F22
         // Character Sheet - In Class Review
         static void Main(string[] args)
         {
+            // Character Name
+            Console.WriteLine("Character Name");
+            // Input
+            Console.WriteLine("Input");
+            // Output
+            Console.WriteLine("Output");
 
+            // Loops
+            Console.WriteLine("Loops");
+
+            // Condonditions
+            Console.WriteLine("Condonditions");
+
+            // Variables
+            Console.WriteLine("Variables");
+
+            // Operators
+            Console.WriteLine("Operators");
+
+            // Stop program from closing
+            Console.ReadKey();
         } // Main
 
     } // class
